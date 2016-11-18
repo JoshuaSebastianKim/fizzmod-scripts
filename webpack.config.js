266fe04470ae9c2f7a0fd98e9ea4f7d764e2cde4
Joshua Sebastian Kim <joshua@fizzmod.com>
2016-11-18 18:45:42 -0300

    Update on dependecies

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,69 @@
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var pug = require("pug-loader");
 
 module.exports = {
-  entry: {
-    main: '../src/main.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: '../src/files'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            require.resolve('babel-preset-es2015'),
-            require.resolve('babel-preset-react')
-          ],
-          plugins: [
-            require.resolve('babel-plugin-transform-runtime'),
-            require.resolve('babel-plugin-transform-es2015-modules-amd')
-          ]
-        }
-      }
-    ]
-  }
+	/**************************/
+	/* ADD CUSTOM CONFIG HERE */
+	entry: {
+		main: '../src/main.js'
+	},
+	output: {
+		filename: '[name].js',
+		path: '../src/files'
+	},
+	/**************************/
+	/* DO NOT EDIT THIS OR YOU WILL GET FIRED */
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			query: {
+				presets: [
+					require.resolve('babel-preset-es2015'),
+					require.resolve('babel-preset-react')
+				],
+				plugins: [
+					require.resolve('babel-plugin-transform-runtime'),
+					require.resolve('babel-plugin-transform-es2015-modules-amd')
+				]
+			}
+		}, {
+			test: /\.css$/,
+			loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1&-autoprefixer!postcss')
+		}, {
+			test: /\.(pug|jade)$/,
+			loader: "pug-loader"
+		}]
+	},
+	resolve: {
+		extensions: ['', '.jsx', '.js', '.json', ".css"],
+		root: [
+			path.resolve(__dirname, "node_modules")
+		]
+	},
+	resolveLoader: {
+		root: path.join(__dirname, 'node_modules'),
+	},
+	postcss: function() {
+		return [
+			autoprefixer({
+				browsers: [
+					'>1%',
+					'last 4 versions',
+					'Firefox ESR',
+					'not ie < 9', // React doesn't support IE8 anyway
+				]
+			}),
+		];
+	},
+	plugins: [
+		new ExtractTextPlugin("style.css", {
+			allChunks: true
+		}),
+		new HtmlWebpackPlugin()
+	]
 }
