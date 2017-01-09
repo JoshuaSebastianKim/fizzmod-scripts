0d39a4009803dd3c8467bc5b5987b18333fd8398
Joshua Sebastian Kim <joshua@fizzmod.com>
2017-01-09 13:40:14 -0300

    Updated config

--- configs/paths.js
+++ configs/paths.js
@@ -1,9 +1,7 @@
 module.exports = {
-	"overlay-delivery-window-promos": '../src/components/overlay-delivery-window-promos/overlay-delivery-window-promos.js',
-	"overlay-calculate-installments": '../src/components/overlay-calculate-installments/overlay-calculate-installments.js',
+	"overlay-calculate-installments": '../src/containers/InstallmentsCalculatorContainer/InstallmentsCalculatorContainer.js',
 	"overlay-paper-gift": '../src/components/overlay-paper-gift/overlay-paper-gift.js',
-	"store-payment-methods": "../src/components/store-payment-methods/store-payment-methods.js",
-	"discounts": "../src/components/Discounts/DiscountContainer.js",
-	"product-item": "../src/components/ProductItem/ProductItemContainer.js",
-	"product-slider": "../src/components/ProductSlider/ProductSliderContainer.js"
+	"store-payment-methods": "../src/containers/PaymentMethodsContainer/PaymentMethodsContainer.js",
+	"store-shipping-methods": "../src/containers/ShippingMethodsContainer/ShippingMethodsContainer.js",
+	"discounts": "../src/containers/DiscountContainer/DiscountContainer.js",
 };
