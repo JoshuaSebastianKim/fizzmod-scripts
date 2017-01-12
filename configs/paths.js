9daee48aa76931602d3a1fbb272a7483807edee5
Joshua Sebastian Kim <joshua@fizzmod.com>
2017-01-12 14:53:17 -0300

    Added path

--- configs/paths.js
+++ configs/paths.js
@@ -3,6 +3,7 @@ module.exports = {
 	"overlay-paper-gift": '../src/components/overlay-paper-gift/overlay-paper-gift.js',
 	"store-payment-methods": "../src/containers/PaymentMethodsContainer/PaymentMethodsContainer.js",
 	"store-shipping-methods": "../src/containers/ShippingMethodsContainer/ShippingMethodsContainer.js",
+	"store-faqs": "../src/containers/FaqsContainer/FaqsContainer.js",
 	"contact-contact": "../src/containers/ContactContainer/ContactContainer.js",
 	"discounts": "../src/containers/DiscountContainer/DiscountContainer.js",
 };
