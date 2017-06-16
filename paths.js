6eed4bc20b9bb21cfc31865871f99308381eced3
Joshua Sebastian Kim <joshuakim150@gmail.com>
2017-06-16 12:04:59 -0300

    Changed paths

--- paths.js
+++ paths.js
@@ -1,17 +1,16 @@
 const path = require('path');
 
 module.exports = {
-	'AR': {
+	AR: {
 		'overlay-calculate-installments': path.resolve(__dirname, '../src/views/InstallmentsCalculatorView.js'),
-		// 'overlay-shipping-preference': path.resolve(__dirname, '../src/views/ShippingPreferenceView.js'),
-		// 'store-payment-methods': path.resolve(__dirname, '../src/views/PaymentMethodsView.js'),
-		// 'store-shipping-methods': path.resolve(__dirname, '../src/views/ShippingMethodsView.js'),
-		// 'store-faqs': path.resolve(__dirname, '../src/views/FaqsView.js'),
-		// 'contact-contact': path.resolve(__dirname, '../src/views/ContactView.js'),
-		// discounts: path.resolve(__dirname, '../src/views/DiscountView.js'),
-		// search: path.resolve(__dirname, '../src/views/SearchView.js'),
+		'store-payment-methods': path.resolve(__dirname, '../src/views/PaymentMethodsView.js'),
+		'store-shipping-methods': path.resolve(__dirname, '../src/views/ShippingMethodsView.js'),
+		'store-faqs': path.resolve(__dirname, '../src/views/FaqsView.js'),
+		'contact-contact': path.resolve(__dirname, '../src/views/ContactView.js'),
+		discounts: path.resolve(__dirname, '../src/views/DiscountView.js'),
+		search: path.resolve(__dirname, '../src/views/SearchView.js'),
 		// home: path.resolve(__dirname, '../src/views/HomeView'),
-		// 'cart-view': path.resolve(__dirname, '../src/views/CartView.js'),
+		'cart-view': path.resolve(__dirname, '../src/views/CartView.js'),
 		vendor: [
 			'react',
 			'react-dom',
@@ -20,7 +19,7 @@ module.exports = {
 			'babel-polyfill'
 		]
 	},
-	'CL': {
+	CL: {
 		vendor: [
 			'react',
 			'react-dom',
@@ -29,7 +28,7 @@ module.exports = {
 			'babel-polyfill'
 		]
 	},
-	'CO': {
+	CO: {
 		// 'overlay-paper-gift': path.resolve(__dirname, '../src/components/overlay-paper-gift/overlay-paper-gift.js'),
 		// 'coverage-search': path.resolve(__dirname, '../src/views/CoverageSearchView.js'),
 		'healthy-world': path.resolve(__dirname, '../src/views/HealthyWorld.js'),
@@ -41,7 +40,7 @@ module.exports = {
 			'babel-polyfill'
 		]
 	},
-	'PE': {
+	PE: {
 		// 'overlay-calculate-installments': path.resolve(__dirname, '../src/views/InstallmentsCalculatorView.js'),
 		// 'overlay-shipping-preference': path.resolve(__dirname, '../src/views/ShippingPreferenceView.js'),
 		// 'store-payment-methods': path.resolve(__dirname, '../src/views/PaymentMethodsView.js'),
