056ec1e646ce1519b795153e662deffde9f1bfce
santiago <santiago@fizzmod.com>
2017-07-19 11:05:44 -0300

    path de PE mi cuenta agregado

--- paths.js
+++ paths.js
@@ -32,6 +32,7 @@ module.exports = {
 		'overlay-paper-gift': path.resolve(__dirname, '../src/components/overlay-paper-gift/overlay-paper-gift.js'),
 		'coverage-search': path.resolve(__dirname, '../src/views/CoverageSearchView.js'),
 		'healthy-world': path.resolve(__dirname, '../src/views/HealthyWorld.js'),
+		'bag-adder': path.resolve(__dirname, '../src/views/BagAdderView.js'),
 		vendor: [
 			'react',
 			'react-dom',
@@ -41,16 +42,8 @@ module.exports = {
 		]
 	},
 	PE: {
-		// 'overlay-calculate-installments': path.resolve(__dirname, '../src/views/InstallmentsCalculatorView.js'),
 		'overlay-shipping-preference': path.resolve(__dirname, '../src/views/ShippingPreferenceView.js'),
-		// 'store-payment-methods': path.resolve(__dirname, '../src/views/PaymentMethodsView.js'),
-		// 'store-shipping-methods': path.resolve(__dirname, '../src/views/ShippingMethodsView.js'),
-		// 'store-faqs': path.resolve(__dirname, '../src/views/FaqsView.js'),
-		// 'contact-contact': path.resolve(__dirname, '../src/views/ContactView.js'),
-		// discounts: path.resolve(__dirname, '../src/views/DiscountView.js'),
-		// search: path.resolve(__dirname, '../src/views/SearchView.js'),
-		// home: path.resolve(__dirname, '../src/views/HomeView'),
-		// 'cart-view': path.resolve(__dirname, '../src/views/CartView.js'),
+		'my-account': path.resolve(__dirname, '../src/views/MyAccountView.js'),
 		vendor: [
 			'react',
 			'react-dom',
