8629d11939908f283e41bcc16c3fbdd8e50a45da
Gonzalo <gonzalo@fizzmod.com>
2017-09-05 11:49:17 -0300

    paths updated

--- paths.js
+++ paths.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
 	'ALL' : {
 		"installments-calculator": path.resolve(__dirname, "../src/views/InstallmentsCalculatorView.js"),
+		"stock-availability": path.resolve(__dirname, "../src/views/StockAvailabilityView.js"),
 
 		vendor: [
 			'react',
